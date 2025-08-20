6d2129effb47b4a675367526219e16e2c8f24369
2823558582@qq.com <2823558582@qq.com>
2025-08-20 11:15:34 +0000

    Created layout; Edited settings and node_out

---
+++ client_code/layout/__init__.py
@@ -0,0 +1,18 @@
+from ._anvil_designer import layoutTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.facebook.auth
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class layout(layoutTemplate):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+        # Any code you write here will run before the form opens.
--- client_code/node_out/__init__.py
+++ client_code/node_out/__init__.py
@@ -15,6 +15,7 @@ class node_out(node_outTemplate):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+        self.repeating_panel_1.items = app_tables.wg_vpn_node.search()
 
     def button_1_click(self, **event_args):
         """This method is called when the button is clicked"""
