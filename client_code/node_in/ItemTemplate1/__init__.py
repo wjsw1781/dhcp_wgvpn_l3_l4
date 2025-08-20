fe68b62342f90ead594df3b5e1c80b3c98083f2d
2823558582@qq.com <2823558582@qq.com>
2025-08-20 03:09:16 +0000

    Created node_in.ItemTemplate1, node_in.ItemTemplate2 and node_in; Edited settings; Deleted login and login_page

--- client_code/node_in/ItemTemplate1/__init__.py
+++ client_code/node_in/ItemTemplate1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import login_pageTemplate
+from ._anvil_designer import ItemTemplate1Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,8 +9,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
-class login_page(login_pageTemplate):
+class ItemTemplate1(ItemTemplate1Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
--- client_code/node_in/__init__.py
+++ client_code/node_in/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import loginTemplate
+from ._anvil_designer import node_inTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,12 +9,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
-class login(loginTemplate):
+class node_in(node_inTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
-        while not anvil.users.get_user():
-            anvil.users.login_with_form()
         # Any code you write here will run before the form opens.
