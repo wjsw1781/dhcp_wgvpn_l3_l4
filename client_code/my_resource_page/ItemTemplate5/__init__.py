be034add80d04c9aab81d160af6c2a85c228faf4
2823558582@qq.com <2823558582@qq.com>
2025-08-20 06:44:03 +0000

    Created my_resource_page.ItemTemplate5; Edited settings and my_resource_page

--- client_code/my_resource_page/ItemTemplate5/__init__.py
+++ client_code/my_resource_page/ItemTemplate5/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import my_resource_pageTemplate
+from ._anvil_designer import ItemTemplate5Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -10,21 +10,12 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class my_resource_page(my_resource_pageTemplate):
+class ItemTemplate5(ItemTemplate5Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
-
-    def repeating_panel_1_show(self, **event_args):
-        """This method is called when the repeating panel is shown on the screen"""
-    
-        me = anvil.users.get_user()
-
-        data = anvil.server.call('show_resource', me)
-        print(data)
-        self.repeating_panel_1.items = data
+        self.label_1.text = self.item['yewu_name']
 
 
-        
