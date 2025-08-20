1edc820662ff045c97c4be3bacc6e19f387c2654
2823558582@qq.com <2823558582@qq.com>
2025-08-20 09:24:35 +0000

    Created node_out.RowTemplate3, node_out.RowTemplate4, node_out.RowTemplate5, node_out.RowTemplate6 and 3 more; Edited settings and node_out

--- client_code/node_out/__init__.py
+++ client_code/node_out/__init__.py
@@ -15,3 +15,15 @@ class node_out(node_outTemplate):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+
+    def button_1_click(self, **event_args):
+        """This method is called when the button is clicked"""
+
+        print(app_tables.wg_vpn_node.list_columns())
+        data = (
+            "wg_host,wg_port,wg_id,wg_account,wg_password,wg_ex_json\n"
+            "1.1.1.1,51820,1,test_user,123456,{\"dns\":\"8.8.8.8\"}\n"
+        ).encode('utf-8')
+        anvil.BlobMedi
+        return BlobMedia("text/csv", data, name="wg_vpn_node_template.csv")
+
